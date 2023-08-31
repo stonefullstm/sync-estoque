bb232d3726843669821805018287f457a57e263a
stonefullstm <stonefull.stm@gmail.com>
2023-08-30 21:10:02 -0300

    fix: Alteração para utilizar postgresql

--- back-end/app/database/db.py
+++ back-end/app/database/db.py
@@ -1,12 +1,25 @@
 from sqlmodel import create_engine, Session, SQLModel
+from sqlalchemy.engine import URL
 
+uri = '''postgres://nynhjpaz:TbMkL0YduVe-
+    npyerTXGiOr0JfyYk1u4@hansken.db.elephantsql.com/nynhjpaz'''
 
-sqlite_file_name = "estoque.sqlite3"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
+url = URL.create(
+    drivername="postgresql",
+    username="nynhjpaz",
+    host="hansken.db.elephantsql.com",
+    password="TbMkL0YduVe-npyerTXGiOr0JfyYk1u4",
+    database="nynhjpaz"
+)
 
-connect_args = {"check_same_thread": False}
+engine = create_engine(url)
 
-engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
+# sqlite_file_name = "estoque.sqlite3"
+# sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+# connect_args = {"check_same_thread": False}
+
+# engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
 
 
 def get_session():
