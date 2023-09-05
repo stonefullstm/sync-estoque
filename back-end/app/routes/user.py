be3f4cb0e53b57e1e4bac1e200197579829663a5
stonefullstm <stonefull.stm@gmail.com>
2023-09-05 17:25:18 -0300

    feat: Criação da rota POST /login para se autenticar na API e obter um token

--- back-end/app/routes/user.py
+++ back-end/app/routes/user.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Depends
-from sqlmodel import Session, select, delete
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlmodel import Session, select
 
 from app.models.user import Usuario
 from app.schemas.user import UserAuth, UserOut
+from app.schemas.token import TokenSchema
 from app.database.db import get_session
 from app.utils.utils import (
     get_hashed_password,
@@ -32,3 +33,30 @@ def create_user(
     session.commit()
     session.refresh(usuario)
     return usuario
+
+
+@router.post(
+        '/login', summary="Create access and refresh tokens for user",
+        response_model=TokenSchema,
+        tags=['Usuário'])
+def login(user: UserAuth, session: Session = Depends(get_session)):
+
+    user_found = session.exec(
+        select(Usuario).where(Usuario.username == user.username)).first()
+    if user_found is None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Incorrect username"
+        )
+
+    hashed_pass = user_found.password
+    if not verify_password(user.password, hashed_pass):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Incorrect password"
+        )
+
+    return {
+        "access_token": create_access_token(user.username),
+        "refresh_token": create_refresh_token(user.username),
+    }
